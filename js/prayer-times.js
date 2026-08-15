cbd4258aa85cb6bb478845e31036c4c36c237978
tamborine996 <tamborine996@users.noreply.github.com>
2026-08-15 20:05:58 +0100

    Refresh prayer-time cache after Asr update

--- js/prayer-times.js
+++ js/prayer-times.js
@@ -47,7 +47,7 @@ const PrayerTimes = (function() {
         }
 
         try {
-            const response = await fetch('data/prayer-times-2026.json?v=20260815-asr1900');
+            const response = await fetch('data/prayer-times-2026.json?v=20260815-asr1900-v2');
             if (!response.ok) {
                 throw new Error('Failed to load prayer times data');
             }
